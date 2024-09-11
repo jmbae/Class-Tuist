219d6a14c2749e2d49c1e4f21c058d018d38a243
Jungman Bae <jungman82@gmail.com>
2024-09-11 11:29:09 +0900

    Initialize Albertos 프로젝트

---
+++ Albertos/Project.swift
@@ -0,0 +1,34 @@
+import ProjectDescription
+
+let project = Project(
+    name: "Albertos",
+    targets: [
+        .target(
+            name: "Albertos",
+            destinations: .iOS,
+            product: .app,
+            bundleId: "io.tuist.Albertos",
+            infoPlist: .extendingDefault(
+                with: [
+                    "UILaunchScreen": [
+                        "UIColorName": "",
+                        "UIImageName": "",
+                    ],
+                ]
+            ),
+            sources: ["Albertos/Sources/**"],
+            resources: ["Albertos/Resources/**"],
+            dependencies: []
+        ),
+        .target(
+            name: "AlbertosTests",
+            destinations: .iOS,
+            product: .unitTests,
+            bundleId: "io.tuist.AlbertosTests",
+            infoPlist: .default,
+            sources: ["Albertos/Tests/**"],
+            resources: [],
+            dependencies: [.target(name: "Albertos")]
+        ),
+    ]
+)
